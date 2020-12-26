62ffc925df39f09e9aef76f9e8a6060d9eee4aa5
Anton Novik <tehbi4.dev@gmail.com>
2020-12-26 20:08:58 +0300

    feat(deploy): implement client configuration (#20)

--- client/services/config/config.ts
+++ client/services/config/config.ts
@@ -1,9 +1,11 @@
+import Constants from 'expo-constants';
+
+const envConfig = Constants.manifest.extra[process.env.NODE_ENV || 'development'];
+
 type Config = {
   API_URL: string;
 };
 
-// TODO add env variables using
 export const config: Config = {
-  API_URL: 'http://localhost:3000',
-  // API_URL: 'https://yocco.ru/api',
+  API_URL: envConfig.API_URL,
 };
