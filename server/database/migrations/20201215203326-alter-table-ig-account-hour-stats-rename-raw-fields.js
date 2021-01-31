da279c5774a7b552f21ac1adcd3297dcf2cbaedf
Anton Novik <anton.novik@open.ru>
2021-01-31 15:29:22 +0300

    fix(migrations): wrap multi query migrations with transaction

--- server/database/migrations/20201215203326-alter-table-ig-account-hour-stats-rename-raw-fields.js
+++ server/database/migrations/20201215203326-alter-table-ig-account-hour-stats-rename-raw-fields.js
@@ -2,30 +2,36 @@
 
 module.exports = {
   up: (queryInterface) =>
-    queryInterface.sequelize.query(
-      `
-        ALTER TABLE "ig_account_hour_stats"
-        RENAME COLUMN "raw_delta_followers_count" TO "raw_follows_count";
+    queryInterface.sequelize.transaction((transaction) =>
+      queryInterface.sequelize.query(
+        `
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "raw_delta_followers_count" TO "raw_follows_count";
 
-        ALTER TABLE "ig_account_hour_stats"
-        RENAME COLUMN "raw_delta_followers_datetime" TO "raw_follows_datetime";
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "raw_delta_followers_datetime" TO "raw_follows_datetime";
 
-        ALTER TABLE "ig_account_hour_stats"
-        RENAME COLUMN "total_folowers_count" TO "total_followers_count";
-      `,
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "total_folowers_count" TO "total_followers_count";
+        `,
+        { transaction },
+      ),
     ),
 
   down: async (queryInterface) =>
-    queryInterface.sequelize.query(
-      `
-      ALTER TABLE "ig_account_hour_stats"
-      RENAME COLUMN "raw_follows_count" TO "raw_delta_followers_count";
+    queryInterface.sequelize.transaction((transaction) =>
+      queryInterface.sequelize.query(
+        `
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "raw_follows_count" TO "raw_delta_followers_count";
 
-      ALTER TABLE "ig_account_hour_stats"
-      RENAME COLUMN "raw_follows_datetime" TO "raw_delta_followers_datetime";
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "raw_follows_datetime" TO "raw_delta_followers_datetime";
 
-      ALTER TABLE "ig_account_hour_stats"
-      RENAME COLUMN "total_followers_count" TO "total_folowers_count";
-    `,
+          ALTER TABLE "ig_account_hour_stats"
+          RENAME COLUMN "total_followers_count" TO "total_folowers_count";
+        `,
+        { transaction },
+      ),
     ),
 };
--- server/database/migrations/20210115205053-recreate-table-user-instagram-accounts.js
+++ server/database/migrations/20210115205053-recreate-table-user-instagram-accounts.js
@@ -2,22 +2,27 @@
 
 module.exports = {
   up: (queryInterface) =>
-    queryInterface.sequelize.query(`
-      DROP TABLE IF EXISTS "user_instagram_accounts";
+    queryInterface.sequelize.transaction((transaction) =>
+      queryInterface.sequelize.query(
+        `
+          DROP TABLE IF EXISTS "user_instagram_accounts";
 
-      CREATE TABLE IF NOT EXISTS "user_instagram_accounts" (
-        "user_id" INTEGER REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-        "ig_account_id" INTEGER REFERENCES "instagram_accounts" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-        "is_default" BOOLEAN DEFAULT NULL,
-        "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
-        "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL,
-        "deleted_at" TIMESTAMP WITH TIME ZONE,
-        UNIQUE ("user_id", "is_default"),
-        UNIQUE ("ig_account_id", "user_id")
-      );
+          CREATE TABLE IF NOT EXISTS "user_instagram_accounts" (
+            "user_id" INTEGER REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+            "ig_account_id" INTEGER REFERENCES "instagram_accounts" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+            "is_default" BOOLEAN DEFAULT NULL,
+            "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+            "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+            "deleted_at" TIMESTAMP WITH TIME ZONE,
+            UNIQUE ("user_id", "is_default"),
+            UNIQUE ("ig_account_id", "user_id")
+          );
 
-      COMMENT ON COLUMN "user_instagram_accounts"."is_default" IS 'It is default instagram account of the user. Can be true or null';
-    `),
+          COMMENT ON COLUMN "user_instagram_accounts"."is_default" IS 'It is default instagram account of the user. Can be true or null';
+        `,
+        { transaction },
+      ),
+    ),
 
   down: () => Promise.resolve(),
 };
