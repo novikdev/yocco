3497c3ab027c04d2be3b745f7da1c24f88bd0601
Anton Novik <tehbi4@gmail.com>
2021-05-10 02:14:51 +0300

    fix(IgAccountStats): (un)followsCount now can be null cause FB issue (part 2)

--- client/screens/IgAccountStats/services/prepareStatsForDisplaying/prepareStatsForDisplaying.ts
+++ client/screens/IgAccountStats/services/prepareStatsForDisplaying/prepareStatsForDisplaying.ts
@@ -3,7 +3,7 @@ import { IHourIgAccountStats } from '@services/api/instagram-accounts';
 export function prepareStatsForDisplaying(stats: IHourIgAccountStats): IHourIgAccountStats {
   let followsCount = null;
   let unfollowsCount = null;
-  if (stats.unfollowsCount && stats.followsCount) {
+  if (typeof stats.unfollowsCount === 'number' && typeof stats.followsCount === 'number') {
     const returnedFollowersCount = stats.unfollowsCount < 0 ? stats.unfollowsCount * -1 : 0;
     unfollowsCount = stats.unfollowsCount + returnedFollowersCount;
     followsCount = stats.followsCount + returnedFollowersCount;
